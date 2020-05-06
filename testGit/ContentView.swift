4676f2a0b97d015d19fdde31f2e1d5776646bcf1
Nico C <nsangohan@gmail.com>
2020-05-06 11:08:50 +0200

    Initial Commit

---
+++ testGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGit
+//
+//  Created by Nicolas Champel on 06/05/2020.
+//  Copyright © 2020 Nicolas Champel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
